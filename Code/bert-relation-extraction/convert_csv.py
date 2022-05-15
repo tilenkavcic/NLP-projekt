423feb37ed52e93bf61999b6a1bd00fd49596729
Aljosa Koren <ak0183@student.uni-lj.si>
2022-05-15 16:54:54 +0200

    new data for bert/experiments

--- Code/bert-relation-extraction/convert_csv.py
+++ Code/bert-relation-extraction/convert_csv.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
 
-DATA_PATH = "./Data/Annotated/EN/TERM_CAT_GEN_SENT_REL.csv"
+DATA_PATH = "./Data/Annotated Train/SL/TERM_CAT_GEN_SENT_REL.csv"
 
-SEM_EVAL_FILE = "./Data/Annotated/EN/SEM_EVAL_FILE_KARST.txt"
+SEM_EVAL_FILE = "./Data/Annotated Train/SL/SEM_EVAL_FILE_KARST.txt"
 
 
 df = pd.read_csv(DATA_PATH)
@@ -38,27 +38,25 @@ def write_to_file(f, s, definendium, relation, relation_name, row_index):
 row_index = 1
 for index, row in df.iterrows():
 
-    genuses = [] if type(row['GENUS']) == float or len(
-        row['GENUS']) <= 0 else row['GENUS'].split("|")
     definendium = row['DEFINIENDUM']
     sentence = row['SENTENCE']
+
+    genuses = [] if type(row['GENUS']) == float or len(
+        row['GENUS']) <= 0 else row['GENUS'].split("|")
     locations = [] if type(row['HAS_LOCATION']) == float or len(
         row['HAS_LOCATION']) <= 0 else row['HAS_LOCATION'].split("|")
     sizes = [] if type(row['HAS_SIZE']) == float or len(
         row['HAS_SIZE']) <= 0 else row['HAS_SIZE'].split("|")
 
-    species = [] if type(row['SPECIES']) == float or len(
-        row['SPECIES']) <= 0 else row['SPECIES'].split("|")
-    occurs = [] if type(row['OCCURS_IN_TIME']) == float or len(
-        row['OCCURS_IN_TIME']) <= 0 else row['OCCURS_IN_TIME'].split("|")
-    results = [] if type(row['HAS_RESULT']) == float or len(
-        row['HAS_RESULT']) <= 0 else row['HAS_RESULT'].split("|")
     forms = [] if type(row['HAS_FORM']) == float or len(
         row['HAS_FORM']) <= 0 else row['HAS_FORM'].split("|")
     functions = [] if type(row['HAS_FUNCTION']) == float or len(
         row['HAS_FUNCTION']) <= 0 else row['HAS_FUNCTION'].split("|")
     cause = [] if type(row['HAS_CAUSE']) == float or len(
         row['HAS_CAUSE']) <= 0 else row['HAS_CAUSE'].split("|")
+
+    composition_medium = [] if type(row['COMPOSITION_MEDIUM']) == float or len(
+        row['COMPOSITION_MEDIUM']) <= 0 else row['COMPOSITION_MEDIUM'].split("|")
 
     row_index = write_to_file(f, sentence, definendium,
                               genuses, "Genus", row_index)
@@ -67,17 +65,12 @@ for index, row in df.iterrows():
     row_index = write_to_file(f, sentence, definendium,
                               sizes, "Has_size", row_index)
     row_index = write_to_file(f, sentence, definendium,
-                              species, "Species", row_index)
-    row_index = write_to_file(f, sentence, definendium,
-                              occurs, "Occurs_in_time", row_index)
-    row_index = write_to_file(f, sentence, definendium,
-                              results, "Has_result", row_index)
-    row_index = write_to_file(f, sentence, definendium,
                               forms, "Has_form", row_index)
     row_index = write_to_file(f, sentence, definendium,
                               functions, "Has_function", row_index)
     row_index = write_to_file(f, sentence, definendium,
                               cause, "Has_cause", row_index)
-
+    row_index = write_to_file(f, sentence, definendium,
+                              composition_medium, "Composition_medium", row_index)
 
 f.close()
